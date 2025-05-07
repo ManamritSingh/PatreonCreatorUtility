55938df43d6778dce7892de6b89321125b710aa4
ARDA7787 <104717701+ARDA7787@users.noreply.github.com>
2025-05-07 00:31:57 -0400

    Add files via upload

--- src/main/java/com/patreon/backend/DemoApplication.java
+++ src/main/java/com/patreon/backend/DemoApplication.java
@@ -3,20 +3,19 @@ package com.patreon.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.patreon.api.models", "com.patreon.backend.models", "com.patreon.utils" })
+@EntityScan(basePackages = {"com.patreon.api.models", "com.patreon.backend.models", "com.patreon.utils"})
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
+    static {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+    }
 
-		// Propagate .env values to system properties so Spring can access them
-		dotenv.entries().forEach(entry ->
-				System.setProperty(entry.getKey(), entry.getValue())
-		);
-
-//		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args); 
+    }
 }
